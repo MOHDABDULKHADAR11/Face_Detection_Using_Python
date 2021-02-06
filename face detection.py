0a718b3ff6a50e5a4382e4b0bf30addd54ebdfa1
MOHDABDULKHADAR11 <52824397+MOHDABDULKHADAR11@users.noreply.github.com>
2021-02-06 15:09:51 +0530

    First Commit

---
+++ face detection.py
@@ -0,0 +1,29 @@
+import cv2,time,numpy
+
+face_cascade = cv2.CascadeClassifier("C:\\Users\\FAZIL\\Desktop\\SDP\\Image Processing - CV\\opencv-master\\opencv-master\\data\\haarcascades\\haarcascade_frontalface_alt.xml")
+
+video = cv2.VideoCapture(0)
+
+a = 1
+
+while True:
+    a=a+1
+    check,frame = video.read()
+    print(frame)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    
+    faces = face_cascade.detectMultiScale(gray,scaleFactor=1.05,minNeighbors=5)
+    
+    for x,y,w,h in faces:
+        frame = cv2.rectangle(frame,(x,y), (x+w,y+h),(0,255,0),3) 
+
+    cv2.imshow("capturing",frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+print(a)
+video.release()
+cv2.destroyAllWindows()
+
